6af67ca30878f447b2729ccc8657f8e7841a7e41
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-01-05 22:03:00 +0300

    Added background, player model and score with label

--- Space Race/GameScene.swift
+++ Space Race/GameScene.swift
@@ -8,13 +8,43 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
-
-    
-    override func didMove(to view: SKView) {
-
+class GameScene: SKScene, SKPhysicsContactDelegate {
+  //MARK: - Properties
+  var starfield: SKEmitterNode!
+  var player: SKSpriteNode!
+  var scoreLabel: SKLabelNode!
+  var score = 0 {
+    didSet {
+      scoreLabel.text = "Score: \(score)"
     }
-    
+  }
+
+  //MARK: - Scene
+    override func didMove(to view: SKView) {
+      backgroundColor = .black
+      starfield = SKEmitterNode(fileNamed: "starfield")!
+      starfield.position = CGPoint(x: 1024, y: 384)
+      starfield.advanceSimulationTime(10)
+      addChild(starfield)
+      starfield.zPosition = -1
+
+      player = SKSpriteNode(imageNamed: "player")
+      player.position = CGPoint(x: 100, y: 384)
+      player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+      player.physicsBody?.contactTestBitMask = 1
+      addChild(player)
+
+      scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+      scoreLabel.position = CGPoint(x: 16, y: 16)
+      scoreLabel.horizontalAlignmentMode = .left
+      addChild(scoreLabel)
+
+      score = 0
+
+      physicsWorld.gravity = .zero
+      physicsWorld.contactDelegate = self
+    }
+
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 
