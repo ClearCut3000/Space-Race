9f92c27c1e508d203f7f2876a1370396906d7438
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-01-05 22:20:24 +0300

    Added Timer.scheduledTimer with @objc func createEnemy that creates randomly objects on scene so that they become obstacles

--- Space Race/GameScene.swift
+++ Space Race/GameScene.swift
@@ -18,9 +18,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
       scoreLabel.text = "Score: \(score)"
     }
   }
+  var possibleEnemies = ["ball", "hummer", "tv"]
+  var gameTimer: Timer?
+  var isGameOver = false
 
   //MARK: - Scene
     override func didMove(to view: SKView) {
+      //Background
       backgroundColor = .black
       starfield = SKEmitterNode(fileNamed: "starfield")!
       starfield.position = CGPoint(x: 1024, y: 384)
@@ -28,12 +32,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
       addChild(starfield)
       starfield.zPosition = -1
 
+      //Player
       player = SKSpriteNode(imageNamed: "player")
       player.position = CGPoint(x: 100, y: 384)
       player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
       player.physicsBody?.contactTestBitMask = 1
       addChild(player)
 
+      //Score label
       scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
       scoreLabel.position = CGPoint(x: 16, y: 16)
       scoreLabel.horizontalAlignmentMode = .left
@@ -41,16 +47,35 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
       score = 0
 
+      //Worlds physics
       physicsWorld.gravity = .zero
       physicsWorld.contactDelegate = self
+
+      gameTimer = Timer.scheduledTimer(timeInterval: 0.35, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
     }
 
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-
-    }
+  //MARK: - UI Methods
+  @objc func createEnemy(){
+    guard let enemy = possibleEnemies.randomElement() else { return }
+    let sprite = SKSpriteNode(imageNamed: enemy)
+    sprite.position = CGPoint(x: 1200, y: Int.random(in: 50...736))
+    addChild(sprite)
+    sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+    sprite.physicsBody?.categoryBitMask = 1
+    sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+    sprite.physicsBody?.angularVelocity = 5
+    sprite.physicsBody?.linearDamping = 0
+    sprite.physicsBody?.angularDamping = 0
+  }
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+      for node in children {
+        if node.position.x < -300 {
+          node.removeFromParent()
+        }
+      }
+      if !isGameOver {
+        score += 1
+      }
     }
 }
